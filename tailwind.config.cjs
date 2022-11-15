92368c1e56b1484371ea43830930f342104c178e
laurent <laurent.blondy1@gmail.com>
2022-11-15 18:01:11 +0100

    gold brightness correction

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -100,7 +100,7 @@ module.exports = {
                '--base-content-focus': '174 100% 98%',
                'base-content': '#c2cdd0',
                // 'base-100': '#02454f',
-               'base-100': 'hsl(182 100% 19%)',
+               'base-100': 'hsl(182 100% 18%)',
                primary: 'gold',
                // primary: 'orange',
                // primary: 'turquoise',
